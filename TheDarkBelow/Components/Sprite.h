c474eb7fd3c2fb846b1dd6789deba69b6306c7d7
Frank van de Voorde <cairhiin@hotmail.com>
2022-12-31 14:05:19 +0200

    Change Sprite component to preserve sfml texture

--- TheDarkBelow/Components/Sprite.h
+++ TheDarkBelow/Components/Sprite.h
@@ -1,12 +1,25 @@
 #ifndef COMPONENT_SPRITE
 #define COMPONENT_SPRITE
 
+#include <iostream>
 #include <SFML/Graphics/Sprite.hpp>
+#include <SFML/Graphics/Texture.hpp>
+#include <SFML/Graphics/Rect.hpp>
+#include "../Constants.h"
 
 namespace DarkBelow {
 	namespace ECS {
 		struct Sprite {
 			sf::Sprite sprite;
+			sf::Texture texture;
+			sf::IntRect srcRect = sf::IntRect(0, 0, 0, 0);
+
+			void init() {
+				this->sprite.setTexture(this->texture);
+				if (srcRect.height != 0) {
+					this->sprite.setTextureRect(srcRect);
+				}
+			}
 		};
 	}
 }
