bf719bd52a415193ed030d3b8f55c1919a4d492e
Frank van de Voorde <cairhiin@hotmail.com>
2023-01-16 18:21:37 +0200

    Add level and name component

---
+++ TheDarkBelow/Components/Level.h
@@ -0,0 +1,12 @@
+#ifndef LEVEL_COMPONENT
+#define LEVEL_COMPONENT
+
+namespace DarkBelow {
+	namespace ECS {
+		struct Level {
+			size_t level;
+		};
+	}
+}
+
+#endif
---
+++ TheDarkBelow/Components/Name.h
@@ -0,0 +1,14 @@
+#ifndef NAME_COMPONENT
+#define NAME_COMPONENT
+
+#include <string>
+
+namespace DarkBelow {
+	namespace ECS {
+		struct Name {
+			std::string name;
+		};
+	}
+}
+
+#endif
